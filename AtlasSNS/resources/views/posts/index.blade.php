0c46a4cb3ba46c00e38341be5ca65494b55ec306
島津清子 <strongest820ks@gmail.com>
2022-12-15 10:52:07 +0900

    TOPページに戻る記述が間違っていたため修正いたしました

--- AtlasSNS/resources/views/posts/index.blade.php
+++ AtlasSNS/resources/views/posts/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.login')
 
 @section('content')
-<h2>機能を実装していきましょう。</h2>
+    <div class="">
+        <p class="pull-right"><a class="btn btn-success" href="post/create-form">投稿する</a></p>
+        <table class="">
 
 @endsection
